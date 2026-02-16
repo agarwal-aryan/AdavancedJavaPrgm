194b8dd6a1d6b04b750302a2b2ccd4ce3a55c177
aryan <aryan.25161053@kiet.edu>
2026-02-17 00:32:13 +0530

    java8 prgms

---
+++ java8/MainFunctionalInterface.java
@@ -0,0 +1,25 @@
+package com.java8;
+
+@FunctionalInterface
+interface Add {
+    int calculate(int n1, int n2);
+//    void calculate(int n1,int n2,int n3);
+}
+
+public class MainFunctionalInterface {
+    public static void main(String[] args) {
+        Add a=new Add() {
+            @Override
+            public int calculate(int n1, int n2) {
+                return 0;
+            }
+
+//            @Override
+            public void calculate(int n1, int n2, int n3) {
+
+            }
+        };
+        Add a1=(int n1,int n2)->(n1+n2);
+        System.out.println(a1.calculate(20,30));
+    }
+}
