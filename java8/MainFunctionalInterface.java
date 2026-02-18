ff4217fb5b4db35b4538ef0bdea145ff86a9ad6c
aryan <aryanagarwal607@gmail.com>
2026-02-18 18:04:18 +0530

    upadted

--- java8/MainFunctionalInterface.java
+++ java8/MainFunctionalInterface.java
@@ -1,26 +1,42 @@
 package com.java8;
 
+import java.util.Scanner;
+
 @FunctionalInterface
 interface Add {
-    int calculate(int n1, int n2);
+    boolean vote(int age);
 //    void calculate(int n1,int n2,int n3);
 }
 
 public class MainFunctionalInterface {
     public static void main(String[] args) {
-        Add a=new Add() {
-            @Override
-            public int calculate(int n1, int n2) {
-                return 0;
-            }
+        Scanner sc=new Scanner(System.in);
+//        Add a=new Add() {
+//            @Override
+//            public void calculate() {
+//                return 0;
+//
+//            }
 
 //            @Override
-            public void calculate(int n1, int n2, int n3) {
-
+//            public void calculate(int n1, int n2, int n3) {
+//
+//            }
+//        };
+        Add a1=(int age)->{
+            if (age>18){
+                return true;
             }
+            return false;
         };
-        Add a1=(int n1,int n2)->(n1+n2);
-        System.out.println(a1.calculate(20,30));
-        System.out.println();
+        int age=sc.nextInt();
+        if(a1.vote(age)){
+            System.out.println("Can vote");
+        }else {
+            System.out.println("Cannote vote");
+        }
+
+//        System.out.println();
+//        System.out.println();
     }
 }
